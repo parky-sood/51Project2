565c4ac13bb6ab9b5c94c55d8d507d1abce02aed
parky-sood <105092514+parky-sood@users.noreply.github.com>
2024-03-06 11:34:19 -0800

    Update gshare.cpp

--- src/gshare.cpp
+++ src/gshare.cpp
@@ -21,16 +21,142 @@
 
 using namespace tinyrv;
 
-GShare::GShare() {
+Scoreboard::Scoreboard(Core* core, uint32_t num_RSs, uint32_t rob_size) 
+  : core_(core)
+  , RS_(num_RSs)
+  , RST_(rob_size, -1) {
   //--
 }
 
-GShare::~GShare() {
+Scoreboard::~Scoreboard() {
   //--
 }
 
-bool GShare::predict(pipeline_trace_t* trace) {
-  __unused (trace);
-  int randomNumber = rand() % 100;
-  return (randomNumber < 75);
+bool Scoreboard::issue(pipeline_trace_t* trace) {
+  auto& ROB = core_->ROB_;
+  auto& RAT = core_->RAT_;
+  
+  // TODO:
+  // check for structural hazards return false if found
+  if (RS_.is_full() || ROB->is_full())
+      return false;
+      
+  // TODO:
+  // load renamed operands (rob1_index, rob2_index) from RAT
+  // use the instruction source operands from trace
+  // a returned value of-1 indicate that the register value is in the register file, otherwise it is in the RS or ROB)
+  int rob1_index = RAT.get(trace->rs1);
+  int rob2_index = RAT.get(trace->rs2);
+   
+  // for each non-available operands (value == -1), obtain their producing RS indices (rs1_index, rs2_index) from the RST
+  // setting rs_index=-1 means the operand value is in the ROB or register File.
+  int rs1_index = (rob1_index != -1) ? RST_[rob1_index] : -1;
+  int rs2_index = (rob2_index != -1) ? RST_[rob2_index] : -1;
+
+  // allocate new ROB entry
+  int rob_index = ROB->allocate(trace);
+
+  // update the RAT if instruction is writing to the register file
+  if (trace->wb) {
+    RAT.set(trace->rd, rob_index);
+  }
+
+  // push trace to RS and obtain index
+  int rs_index = RS_.push(trace, rob_index, rs1_index, rs2_index);
+
+  // update the RST with newly allocated RS index
+  RST_[rob_index] = rs_index;
+
+  return true;
+}
+
+std::vector<pipeline_trace_t*> Scoreboard::execute() {
+  std::vector<pipeline_trace_t*> traces;
+  auto& FUs = core_->FUs_;
+
+  // TODO:
+  // search the RS for any valid and not yet running entry
+  // that is ready (i.e. both rs1_index and rs2_index are -1)
+  // send it to its corresponding FUs
+  // mark it as running
+  // add its trace to return list
+  for (int i = 0; i < (int)RS_.size(); ++i) { 
+    auto& rs_entry = RS_[i];
+    // HERE!
+    if(rs_entry.valid && !rs_entry.running) {
+      if (rs_entry.rs1_index == -1 && rs_entry.rs2_index == -1) {
+        // create a FU entry with same parameters as rs_entry
+        FunctionalUnit::entry_t fu_entry{rs_entry.trace, rs_entry.rob_index, i};
+        // fu_entry.trace = rs_entry.trace;
+        // fu_entry.rob_index = rs_entry.rob_index;
+        // fu_entry.rs_index = i;
+
+        // send FU entry to corresponding FU
+        FUs[static_cast<int>(rs_entry.trace->fu_type)]->Input.send(fu_entry);
+        
+        // set rs_entry to running
+        rs_entry.running = true;
+
+        // push rs_entry trace to list of running traces
+        traces.push_back(rs_entry.trace);
+      }
+    }      
+  }
+
+  return traces;
+}
+
+pipeline_trace_t* Scoreboard::writeback() {
+  pipeline_trace_t* trace = nullptr;
+  auto& ROB = core_->ROB_;  
+  auto& FUs = core_->FUs_;
+
+  // process the first FU to have completed execution by accessing its output
+  for (auto& fu : FUs) {
+    if (fu->Output.empty())
+      continue;
+
+    auto& fu_entry = fu->Output.front();
+
+    // TODO:
+    // broadcast result to all RS pending for this FU's rs_index
+    // invalidate matching rs_index by setting it to -1 to imply that the operand value is now available
+    for (uint32_t i = 0; i < RS_.size(); ++i) { 
+      auto& rs_entry = RS_[i];
+      if (!rs_entry.valid)
+        continue;
+      // HERE!
+      if (rs_entry.rs1_index == fu_entry.rs_index)
+        rs_entry.rs1_index = -1;
+      if (rs_entry.rs2_index == fu_entry.rs_index)
+        rs_entry.rs2_index = -1;
+    }
+    
+    // TODO: 
+    // clear RST by invalidating current ROB entry to -1
+    RST_[fu_entry.rob_index] = -1;
+
+    // TODO: 
+    // notify the ROB about completion (using ROB->Completed.send())
+    ROB->Completed.send(fu_entry.rob_index);
+
+    // TODO: 
+    // deallocate the RS entry of this FU
+    RS_.remove(fu_entry.rs_index);
+    
+    // set the returned trace
+    trace = fu_entry.trace;
+
+    // remove FU entry
+    fu->Output.pop();
+
+    // we process one FU at the time
+    break;
+  }
+
+  return trace;
+}
+
+void Scoreboard::dump() {
+  RS_.dump();
 }
